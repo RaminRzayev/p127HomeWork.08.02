bb4326615eaf80137ad3b15233bab6e62584c9bf
RaminRzayev <raminag@code.edu.az>
2022-02-09 03:38:49 +0400

    add.H.W

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace p127HomeWork08._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           /* 1.Console ekraninda daxil olunmus her hansi bir string-də "a" hərfinin olub-olmamasını ekrana yazdırın.*/
+
+         /*   string name = Console.ReadLine();
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i] =='a')
+                {
+                    Console.WriteLine("yazilmis stringde a herfi tapildi");
+                    break;
+                }
+
+            }*/
+           
+
+
+            
+            // 2. Console ekraninda daxil olunan ededin cut ve ya tek oldugunu ekrana cixardan bir alqoritm yazin
+
+            /* int number = Convert.ToInt32(Console.ReadLine());
+             if (number%2==0)
+             {
+                 Console.WriteLine("eded cutdur");
+             }
+             else
+             {
+                 Console.WriteLine("eded tekdir");
+             }*/
+        }
+    }
+}
